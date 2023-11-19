6812f35f528edd19df5d6bdfc58b112a1d00bb3e
enzo2346 enzo2346 <enzo.gallos@edu.ece.fr>
2023-11-19 22:44:40 +0000

    add gitpod configuration files

--- website_page/app.py
+++ website_page/app.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 
 
 # Create flask routes
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello, World!'
+    return "Hello, World!"
 
 
 # create new route
-@app.route('/about')
+@app.route("/about")
 def about():
-    name = 'Duc'
-    location = 'Ha Noi'
-    return f'My name is {name}, I live in {location}'
+    name = "Duc"
+    location = "Ha Noi"
+    return f"My name is {name}, I live in {location}"
 
 
 # Main function
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
